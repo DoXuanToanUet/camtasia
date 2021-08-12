9bfc6e2de112c46e96af8c74f9e460c6707544f3
Toàn Đỗ <toanwordpresstheme@gmail.com>
2021-08-12 09:54:42 +0700

    update icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
+  new WOW().init();
   console.log("start");
   $("#btn-menu-responsive").click(function () {
     $(".m-menu").toggleClass("active");
     $("body").toggleClass("no-scroll");
   });
 });
-new WOW().init();
